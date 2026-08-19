475ac6daa6e770bfcfd148622e4ebc0cafc39c16
Marc San <sanchezpe.marc@gmail.com>
2026-08-19 15:35:01 +0200

    Finish artifacts and storer clases

--- src/index.js
+++ src/index.js
@@ -1,177 +1,136 @@
 import "./style.css";
 
 class TODO {
- 
-    #id
-    #finalized
-    #name
-    #description
-    #date
-    #priority
 
     constructor(id, name, description, date, priority) {
-        this.#id = id;
-        this.#finalized = false;
-        this.#name = name;
-        this.#description = description;
-        this.#date = date;
-        this.#priority = priority
-    }
-
-    get config() {
-
-        return {
-            id: this.#id,
-            finalized: this.#finalized,
-            name: this.#name,
-            description: this.#description,
-            date: this.#date,
-            priority: this.#priority
-        }
-    }
-
-    set config(newData) {
-        this.#id = newData.id ? newData.id : this.#id;
-        this.#finalized = newData.finalized ? newData.finalized : this.#finalized;
-        this.#name = newData.name ? newData.name : this.#name;
-        this.#description = newData.description ? newData.description : this.#description;
-        this.#date = newData.date ? newData.date : this.#date;
-        this.#priority = newData.priority ? newData.priority : this.#priority;
-        
+        this.id = id;
+        this.finalized = false;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.priority = priority
     }
 
 }
 
 class Project {
- 
-    #id
-    #name
-    #description
-    #date
 
-    constructor(id, name, description, date, priority) {
-        this.#id = id;
-        this.#name = name;
-        this.#description = description;
-        this.#date = date;
-    }
-
-    get config() {
-
-        return {
-            id: this.#id,
-            name: this.#name,
-            description: this.#description,
-            date: this.#date,
-        }
-    }
-
-    set config(newData) {
-        this.#id = newData.id ? newData.id : this.#id;
-        this.#name = newData.name ? newData.name : this.#name;
-        this.#description = newData.description ? newData.description : this.#description;
-        this.#date = newData.date ? newData.date : this.#date;
-        
+    constructor(id, name, description, date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
     }
 
 }
 
 class Notes {
- 
-    #id
-    #name
-    #description
 
     constructor(id, name, description, date, priority) {
-        this.#id = id;
-        this.#name = name;
-        this.#description = description;
-    }
-
-    get config() {
-
-        return {
-            id: this.#id,
-            name: this.#name,
-            description: this.#description,
-        }
-    }
-
-    set config(newData) {
-        this.#id = newData.id ? newData.id : this.#id;
-        this.#name = newData.name ? newData.name : this.#name;
-        this.#description = newData.description ? newData.description : this.#description;
-        
+        this.id = id;
+        this.name = name;
+        this.description = description;
     }
 
 }
 
 class Storer {
 
-    #todos
-    #projects
-    #notes
-    #currentTab
+    currentTab
 
-    get todos() {
-        return this.#todos;
-    }
+    mapper = {
+        'todo': 'todos',
+        'project': 'projects',
+        'note': 'notes',
 
-    set todos(todo) {
-        this.#todos.push(todo)
-    }
-
-    get projects() {
-        return this.#projects;
-    }
-
-    set projects(project) {
-        this.#projects.push(project)
-    }
-
-    get notes() {
-        return this.#notes;
-    }
-
-    set notes(note) {
-        this.#notes.push(note)
-    }
+    };
 
     get currentTab() {
-        return this.#currentTab;
+        return this.currentTab;
     }
 
     set currentTab(tab) {
-        this.#currentTab.push(tab)
+        this.currentTab.push(tab);
     }
 
-    deleteItem(itemType, id) {
-        if (itemType === 'todo') {
-            const todoItem = this.#todos.filter( todo => todo.id === id )
-            if (todoItem.length > 0) {
-                delete this.#todos.todoItem[0];
-            }
+    static #updatedList(itemType, item) {
+        const itemStorage = this.mapper[itemType];
+        const listObj = JSON.parse(localStorage.getItem(itemStorage));
+        listObj.push(item);
+        return JSON.stringify(listObj);
+    }
+    
+    static saveItem(itemType, item) {
 
-            console.log(`[Storer]: No se ha encontrado el id del TODO.`)
+        const itemStorage = this.mapper[itemType];
+
+        if (!localStorage.getItem(itemStorage)) {
+            localStorage.setItem(itemStorage, JSON.stringify([]));
         }
 
-        else if (itemType === 'project') {
-            const projectItem = this.#projects.filter( project => project.id === id )
-            if (projectItem.length > 0) {
-                delete this.#projects.projectItem[0];
-            }
-
-            console.log(`[Storer]: No se ha encontrado el id del Project.`)
+        try {
+            const listStr = Storer.#updatedList(itemType, item);
+            localStorage.setItem(itemStorage, listStr);
+            console.log(`[Storer]: ${itemType} almacenado correctamente.`);
         }
 
-        else if (itemType === 'note') {
-            const noteItem = this.#notes.filter( note => note.id === id )
-            if (noteItem.length > 0) {
-                delete this.#notes.noteItem[0];
-            }
-
-            console.log(`[Storer]: No se ha encontrado el id de la Note.`)
+        catch {
+            console.error(`[Storer]: No se ha podido almacenar el ${itemType}.`);
         }
     }
+    
+    static getItem(itemType, id) {
 
+        try {
+            const itemStorage = this.mapper[itemType];
+
+            const listObj = JSON.parse(localStorage.getItem(itemStorage));
+            const filteredList = listObj.filter( t => t.id === itemId );
+
+            console.log(`[Storer]: ${itemType} ${id} obtenido correctamente.`);
+            return filteredList[0];
+        }
+
+        catch {
+            console.error(`[Storer]: No se ha podido obtener el ${itemType} ${id}.`);
+        }
+
+    }
+
+    static deleteItem(itemType, id) {
+
+        try {
+            const itemStorage = this.mapper[itemType];
+
+            const listObj = JSON.parse(localStorage.getItem(itemStorage));
+            const filteredList = JSON.stringify(
+                listObj.filter(
+                    t => t.id !== itemId
+                )
+            );
+
+            localStorage.setItem(itemStorage, filteredList);
+        
+            console.log(`[Storer]: ${itemType} ${id} eliminado correctamente.`);
+        }
+
+        catch {
+            console.error(`[Storer]: No se ha podido eliminar el ${itemType} ${id}.`);
+        }
+
+    }
+
+    static getItems(itemType) {
+        const itemStorage = this.mapper[itemType];
+
+        try {
+            console.log(`[Storer]: ${itemStorage} obtenidos correctamente.`);
+            return JSON.parse(localStorage.getItem(itemStorage));
+        }
+
+        catch{
+            console.error(`[Storer]: No se han podido obtener los ${itemStorage}.`);
+        }
+
+    }
 }
